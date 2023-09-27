8c1bb3c7db882e96deb87759cd82de0f8bf149d6
yu-heejin <jkgt0521@naver.com>
2023-09-28 01:47:19 +0900

    Revert ":sparkles:  feat: 테스트용 화면 추가"
    This reverts commit 80a9b1aa43036b9e94ca4af4a2f6fd1589f3586a.

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -1,27 +1,16 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: function (origin: any, callback: any): void {
-        if (origin === 'http://localhost:3000') {
-            callback(null, true); // 허용
-        } else {
-            callback(new Error('Not allowed by CORS')); // 거부
-        }
-    }
-}));
+app.use(express.json())
 
-app.post('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     console.log(req.body);
     
     res.status(200).send();
 });
 
-app.listen('3001', () => {
+app.listen('3000', () => {
     console.log(`
         ################################################
         welcome to com-ha !
