80a9b1aa43036b9e94ca4af4a2f6fd1589f3586a
yu-heejin <jkgt0521@naver.com>
2023-09-28 01:45:18 +0900

    :sparkles:  feat: 테스트용 화면 추가

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -1,16 +1,27 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: function (origin: any, callback: any): void {
+        if (origin === 'http://localhost:3000') {
+            callback(null, true); // 허용
+        } else {
+            callback(new Error('Not allowed by CORS')); // 거부
+        }
+    }
+}));
 
-app.get('/', (req: Request, res: Response) => {
+app.post('/', (req: Request, res: Response) => {
     console.log(req.body);
     
     res.status(200).send();
 });
 
-app.listen('3000', () => {
+app.listen('3001', () => {
     console.log(`
         ################################################
         welcome to com-ha !
