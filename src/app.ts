8626e468e99fe11cf43760c0adacc2cdfdf3492e
yu-heejin <jkgt0521@naver.com>
2023-09-28 01:05:18 +0900

    :sparkles:  feat: keyword type 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(200).send();
+});
 
 app.listen('3000', () => {
     console.log(`
---
+++ src/keyword.ts
@@ -0,0 +1,33 @@
+export type VariableKeyword = {
+    variable: '학생 이거 뭘까?'
+    constVariable: '학생 왜 이걸 모르지?',
+    variableUse: '학생',
+    assignment: '학생?',
+};
+
+export type LoopKeyword = {
+    loopStart: '뭐 나도 맨날 헷갈리는데',
+    loopEnd: '하여튼 이거 알아둬요',
+};
+
+export type OperatorKeyword = {
+    plus: '+',
+    minus: '-',
+    divide: '/',
+    multiple: '*',
+    remain: '%',
+    operator: '하도록 하세요.'
+};
+
+export type NumberKeyword = {
+    numberStart: '왜 대답이 없지',
+};
+
+export type StringKeyword = {
+    stringStart: '늬들 선배중에',
+    stringEnd: '그런 선배가 있었어',
+};
+
+export type ConsoleKeyword = {
+    print: '집에 가서 꼭 해보도록 합시다.',
+};
