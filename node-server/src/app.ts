91fe9d5b4cd8bbf77ac90a2ea61f9a1fa6439f62
yu-heejin <jkgt0521@naver.com>
2023-09-28 02:01:34 +0900

    :sparkles:  feat: 반복문 검증 기능 추가

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -16,8 +16,26 @@ app.use(cors({
 }));
 
 app.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    
+    const { text } = req.body;
+    const arr = text.split('\n');
+
+    for (const a of arr) {
+        const token = a as string;
+        // 변수가 있는지 확인하기
+
+        // 반복문인지 확인하기
+        if (token.includes('뭐 나도 맨날 헷갈리는데')) {
+            let loopCount = 0;
+            for (let x of token) {
+                if (x === '~') loopCount++;
+            }
+
+            for (let i = 0; i < loopCount; i++) {
+                
+            }
+        }
+    }
+
     res.status(200).send();
 });
 
